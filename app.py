f0b9fb09f11f8814c1a463b6b5a3041a73b834b2
MishkaSimakov <msimakov661@gmail.com>
2024-03-19 20:04:41 +0300

    prepared for hosting

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 from flask import Flask
 
-app = Flask(__name__)
+application = Flask(__name__)
 
 
-@app.route('/')
+@application.route('/')
 def hello():
     return "hello!"
+
+
+if __name__ == "__main__":
+    application.run(host='0.0.0.0')
